34e1cef55feca53128e0a9030466502be89b6f1e
David Frischknecht <fishnet37222@gmail.com>
2020-11-17 13:38:17 -0500

    Initial Commit

---
+++ My Coaster Tracker/My_Coaster_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Coaster_TrackerApp.swift
+//  My Coaster Tracker
+//
+//  Created by David Frischknecht on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct My_Coaster_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
